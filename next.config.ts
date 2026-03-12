03c22b0ea530ae1f7fabe18ff9e0f726e0eb2f8e
nonsanwin-debug <232249562+nonsanwin-debug@users.noreply.github.com>
2026-03-12 16:35:40 +0900

    fix: injecting Kakao API Key fallback for Production via next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://zfaagizkdixopldhqixv.supabase.co',
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmYWFnaXprZGl4b3BsZGhxaXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzMDA1MTQsImV4cCI6MjA4NTg3NjUxNH0.SnQXSR0AO44NeWO0xv1WdOZ8dfJrn6pKRiJWOHPrOWI',
+    NEXT_PUBLIC_KAKAO_MAP_KEY: process.env.NEXT_PUBLIC_KAKAO_MAP_KEY || 'c9b7bd6fa67ee5f5724b76fa58d72ecc',
   },
   images: {
     remotePatterns: [
